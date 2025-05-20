4224c98adb4caeced1771bb9cd5df778ea504340
unknown <mjrjuyel8@gmail.com>
2025-05-20 23:32:30 +0600

    push

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+       Route::get('/', function () {
     return view('welcome');
 });
 
@@ -18,3 +20,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+    });
+}
